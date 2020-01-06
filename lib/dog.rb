fbd8ad7c74ead4512321fc46625f54184aa629cc
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-06 01:44:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog 
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all = save
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save
+    @save
+    @@all << self 
+  end 
+   
+  def self.clear_all
+    @@all.clear 
+  end 
+  
+  def self.print_all
+    self.all.each do |d|
+      print d 
+    end
+   # print_all
+  end 
+end 
